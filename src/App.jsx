2388e506a7ccfb8c784e336c57ebcb51b57f6450
neiltejada <Neiltejada86@gmail.com>
2024-06-19 17:53:13 -0700

    props cont.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import * as React from "react";
 
 const App = () => {
@@ -19,67 +20,54 @@ const App = () => {
       objectId: 1,
     },
   ];
-  console.log("App renders");
+
+  const [searchTerm, setSearchTerm] = React.useState("React");
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search />
+      <Search search={searchTerm} onSearch={handleSearch} />
 
       <hr />
 
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>
   );
 };
 
-const Search = () => {
-  const [searchTerm, setSearchTerm] = React.useState(" ");
-  console.log("Search renders");
-
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-    // synthetic event
-    console.log(event);
-    // value of target (here: input HTML element)
-    console.log(event.target.value);
-  };
-
-  return (
-    <div>
-      <label htmlFor="search">Search:</label>
-      <input id="search" type="text" onChange={handleChange} />
-      <p>
-        Searching for <strong>{searchTerm}</strong>
-      </p>
-    </div>
-  );
+const Search = ({ search, onSearch }) => {
+  <div>
+    <label htmlFor="search">Search:</label>
+    <input id="search" type="text" value={search} onChange={onSearch} />
+  </div>;
 };
 
-const List = (props) => {
-  console.log("List renders");
-  return (
-    <ul>
-      {props.list.map((item) => (
-        <Item key={item.objectId} item={item} />
-      ))}
-    </ul>
-  );
-};
+const List = ({ list }) => (
+  <ul>
+    {list.map((item) => (
+      <Item key={item.objectId} item={item} />
+    ))}
+  </ul>
+);
 
-const Item = (props) => {
-  console.log("Item renders");
-  return (
-    <li>
-      <span>
-        <a href={props.item.url}>{props.item.title}</a>
-      </span>
-      <span>{props.item.author}</span>
-      <span>{props.item.num_comments}</span>
-      <span>{props.item.points}</span>
-    </li>
-  );
-};
+const Item = ({ item }) => (
+  <li>
+    <span>
+      <a href={item.url}>{item.title} </a>
+    </span>
+    <span>{item.author}, </span>
+    <span>{item.num_comments}, </span>
+    <span>{item.points}</span>
+  </li>
+);
 
 export default App;
